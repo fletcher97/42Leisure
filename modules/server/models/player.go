96719679d1edd13d4484d3e5bf27ecc34c981f1d
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-09-02 04:02:27 +0100

    Added player and stats models for db

---
+++ modules/server/models/player.go
@@ -0,0 +1,28 @@
+package models
+
+import "gorm.io/gorm"
+
+const (
+	Player_loaded msgType = iota
+	Player_created
+)
+
+type Player struct {
+	gorm.Model
+	Name  string `gorm:"uniqueIndex"`
+	Stats []GameStats
+}
+
+type GameStats struct {
+	gorm.Model
+	PlayerID uint
+
+	Name   string
+	Wins   uint32
+	Losses uint32
+	Draws  uint32
+}
+
+func (p1 Player) Equals(p2 Player) bool {
+	return p1.ID == p2.ID
+}
