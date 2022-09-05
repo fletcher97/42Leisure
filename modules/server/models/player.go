160220c38ecb7fe78569cfc4d1626acf33c25823
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-09-05 14:28:11 +0100

    Added not null qualifier for some fields

--- modules/server/models/player.go
+++ modules/server/models/player.go
@@ -9,15 +9,15 @@ const (
 
 type Player struct {
 	gorm.Model
-	Name  string `gorm:"uniqueIndex"`
+	Name  string `gorm:"uniqueIndex;not null"`
 	Stats []GameStats
 }
 
 type GameStats struct {
 	gorm.Model
-	PlayerID uint
+	PlayerID uint `gorm:"not null"`
 
-	Name    string
+	Name    string `gorm:"not null"`
 	Wins    uint32
 	Losses  uint32
 	Draws   uint32
